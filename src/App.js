770400229522bcee542691aab5b9c40c31c3cd40
KostaLaz <kosta.lazarevski89@gmail.com>
2021-08-08 21:28:50 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Todo from './componrnts/Todo';
-import Backdrop from './componrnts/Backdrop';
-import Modal from './componrnts/Modal';
 
 function App() {
   return (
     <div className="App">
     <Todo text="Title"/>
     <Todo text="CARD" />
-    <Modal />
-    <Backdrop />
     </div>
   );
 }
