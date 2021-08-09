90ceefb4bbd5569dc7231ed389d223d0a5c421a2
KostaLaz <kosta.lazarevski89@gmail.com>
2021-08-09 23:49:00 +0200

    cancelModalHandler

--- src/componrnts/Modal.js
+++ src/componrnts/Modal.js
@@ -5,15 +5,19 @@ const Modal = (props) => {
 
     const [ modalIsOpen, setModalIsOpen ] = useState(false);
 
-    const deleteButtonHandler = () => {
+    const onConfirmHandler = () => {
         setModalIsOpen(false);
+    };
+
+    const onCancelHandler = () => {
+        props.onCancelHandler();
     };
 
     return (
         <div className="modal">
             <p>Are you sure?</p>
             <button className="btn" >Confirm</button>
-            <button className="btn btn--alt" onClick={props.onCancelHandler}>Cancel</button>
+            <button className="btn btn--alt" onClick={onCancelHandler}>Cancel</button>
         </div>
     );
 }
