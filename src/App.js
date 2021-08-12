185028da211afdf5f8983a1e6c145a9035281f72
KostaLaz <kosta.lazarevski89@gmail.com>
2021-08-13 01:29:07 +0200

    Lyout wrapper

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetupsPage';
 import FavoritesPage from './pages/FavoritesPage';
 import NewMeetupPage from './pages/NewMeetupPage';
-
+import Layout from './components/layouts/Layout';
 
 function App() {
   return (
-   <div>
-     <Switch>
-     <Route path='/' exact>
-       <AllMeetupsPage />
-     </Route>
-     <Route path='/new-meetup' >
-       <NewMeetupPage />
-     </Route>
-     <Route path='/favorites' >
-       <FavoritesPage />
-     </Route>
-     </Switch>
-   </div>
+    <Layout>
+      <Switch>
+      <Route path='/' exact>
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/new-meetup' >
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favorites' >
+        <FavoritesPage />
+      </Route>
+      </Switch>
+    </Layout>
+   
   );
 }
 
