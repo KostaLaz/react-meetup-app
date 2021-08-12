1cf4472c3bcf0ae01b25334eeef574e0a46ddcfa
KostaLaz <kosta.lazarevski89@gmail.com>
2021-08-13 01:19:01 +0200

    card wrapper added to meetup item

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import classes from './MeetupItem.module.css';
+import Card from '../ui/Card';
 
 
 function MeetupItem(props) {
     return (
         <li className={classes.item}>
+            <Card>
         <div className={classes.image}>
             <img src={props.image} alt={props.title} />
         </div>
@@ -16,6 +18,7 @@ function MeetupItem(props) {
         <div className={classes.actions}>
             <button>To Favorites</button>
         </div>
+        </Card>
         </li>
     )
 }
--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import classes from './Card.module.css';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className={classes.card}>
-            
+            {props.children}
         </div>
     )
 }
