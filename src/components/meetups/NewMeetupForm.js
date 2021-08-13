6a470efa70f882f3c364d0f4f830005682a3fd82
KostaLaz <kosta.lazarevski89@gmail.com>
2021-08-13 02:14:22 +0200

    submit handler

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -3,9 +3,14 @@ import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
 function NewMeetupForm() {
+
+    const submitHandler = (event) => {
+        
+    };
+
     return (
        <Card>
-           <form className={classes.form}>
+           <form className={classes.form} onSubmit={submitHandler}>
              <div className={classes.control}>
                 <label htmlFor="title">Meetup Title</label>
                 <input type="text" required id="title" />
