327cfe4075f02c8dc5ec816c89cdfe65bdddf5c2
KostaLaz <kosta.lazarevski89@gmail.com>
2021-08-14 22:26:11 +0200

    get form input values

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -5,8 +5,19 @@ import classes from './NewMeetupForm.module.css';
 
 function NewMeetupForm() {
 
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const adressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
     const submitHandler = (event) => {
         event.preventDefault();
+
+        const titleEntered = titleInputRef.current.value;
+        const imageEntered = imageInputRef.current.value;
+        const adressEntered = descriptionInputRef.current.value;
+        const decsriptionEntered = descriptionInputRef.current.value;
+
     };
 
     return (
@@ -14,19 +25,19 @@ function NewMeetupForm() {
            <form className={classes.form} onSubmit={submitHandler}>
              <div className={classes.control}>
                 <label htmlFor="title">Meetup Title</label>
-                <input type="text" required id="title" />
+                <input type="text" required id="title" ref={titleInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor="image">Meetup Image</label>
-                <input type="url" required id="image" />
+                <input type="url" required id="image" ref={imageInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor="adress">Adress</label>
-                <input type="text" required id="adress" />
+                <input type="text" required id="adress" ref={adressInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor="decription">Description</label>
-                <textarea required id="decription" roes='5' />
+                <textarea required id="decription" roes='5' ref={descriptionInputRef} />
             </div>
             <div className={classes.actions}>
                 <button>Add Meetup</button>
