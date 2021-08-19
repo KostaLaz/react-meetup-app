a9eed2430edd34ca86ac5f39c6ee4760a1ac8e1c
KostaLaz <kosta.lazarevski89@gmail.com>
2021-08-19 20:39:40 +0200

    get all favorites

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import FavoritesContext from '../store/favorites-context'
 import MeetupList from '../components/meetups/MeetupList';
 
@@ -8,11 +8,33 @@ function FavoritesPage() {
     const favoritesContext = useContext(FavoritesContext);
 
     let content;
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedFavorites, setLoadedFavorites] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(
+          'https://meetup-app-4c489-default-rtdb.firebaseio.com/favorites.json'
+        ).then(response => {
+          return response.json();
+        }).then(data => {
+          const favorites = [];
+          for (const key in data) {
+            const favorite = {
+              id: key,
+              ...data[key]
+            }
+            favorites.push(favorite);
+          }
+          setIsLoading(false);
+          setLoadedFavorites(favorites);
+        });
+      }, []);
 
     if (favoritesContext.totalFavorites === 0) {
         content = <p>You got no favorites yet. Start adding some?</p>
     } else {
-        <MeetupList meetups={favoritesContext.favorites} />
+       content = <MeetupList meetups={favoritesContext.favorites} />
     }
 
     return (
